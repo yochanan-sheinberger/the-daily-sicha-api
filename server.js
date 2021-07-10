e1ee0f0ae63e97712413c949f90a2084ed709371
Yochanan Sheinberger <yochkesh@gmail.com>
2021-07-10 23:34:27 +0300

    11th commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
-
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 require('./config/passport')(app);
